ce2343d3ace7d3ed7e09d223ac24b85897e7d441
Charles Guthrie <charles.guthrie3@gmail.com>
2017-02-21 16:01:11 -0500

    edited fb_post_liwc function with a conditional statement to handle df with or without the 'type' variable

--- doing_things_with_data/doing_things_with_R/scripts_r/project_helper_functions/liwc_projects_helper_funtions/liwc_fb_processing_functions.R
+++ doing_things_with_data/doing_things_with_R/scripts_r/project_helper_functions/liwc_projects_helper_funtions/liwc_fb_processing_functions.R
@@ -1,13 +1,13 @@
 ##helper functions for working with LIWC output csv files
 
 fb_post_liwc <- function(file, stop_column){
-  data <- read.csv(file = file, header = T, stringsAsFactors = F) 
-  colnames(data)[1:which(colnames(data)==stop_column)-1] <- data[1,1:which(colnames(data)==stop_column)-1]
-  data <- data[-1,]
+  data <- read.csv(file = file, header = T, stringsAsFactors = F) #read in liwc output csv 
+  colnames(data)[1:which(colnames(data)==stop_column)-1] <- data[1,1:which(colnames(data)==stop_column)-1] #adjust colnames from first row data
+  data <- data[-1,] #remove first row with colname information 
   data$created_time <- as.Date(data$created_time)
-  data$type <- as.factor(data$type)
+  if("type" %in% colnames(data)){data$type <- as.factor(data$type)} #if a comments dataset is used there will be no "type" variable
   for(i in grep(pattern = "_count", colnames(data))){
-    data[,colnames(data[i])] <- as.numeric(data[,colnames(data[i])])
+    data[,colnames(data[i])] <- as.numeric(data[,colnames(data[i])]) #convert reaction and comment variables to numeric class
   }
   return(data)
 }
