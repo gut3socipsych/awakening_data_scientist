e72b153f62c6767ac2e89f7632739d5ae4303a1e
Charles Guthrie <charles.guthrie3@gmail.com>
2017-04-08 10:01:06 -0400

    updates to slr by hand py and jules files

--- doing_things_with_data/doing_things_with_Py/scripts_py/analyzing_data/linear_regressions/simple_linear_regression_manual_calculation.py
+++ doing_things_with_data/doing_things_with_Py/scripts_py/analyzing_data/linear_regressions/simple_linear_regression_manual_calculation.py
@@ -9,23 +9,17 @@ y = [10,12,22,27,14,28,32,27,20,10]
 x_mean = sum(x)/len(x)
 y_mean = sum(y)/len(y)
 
-##deviance scores
-#x deviance scores
-x_dev = []
-for i in x: x_dev += [i - x_mean]
-#y deviance scores
-y_dev = []
-for i in y: y_dev += [i - y_mean]
-#x deviance scores squared
-x_dev2 = []
-for i in x_dev: x_dev2 += [i**2]
-#y deviance scores squared
-y_dev2 = []
-for i in y_dev: y_dev2 += [i**2]
-#product of x deviance and y deviance scores
-xy_dev = []
-for i in range(0, len(x_dev)): xy_dev += [x_dev[i] * y_dev[i]]
-
+##deviation scores
+#x deviation scores
+x_dev = [i - x_mean for i in x]
+#y deviation scores
+y_dev = [i - y_mean for i in y]
+#x deviation scores squared
+x_dev2 = [i**2 for i in x_dev]
+#y deviation scores squared
+y_dev2 = [i**2 for i in y_dev]
+#product of x deviation and y deviation scores
+xy_dev = [xvalue * y_dev[num] for num, xvalue in enumerate(x_dev)]
 
 ###estimating the coefficients
 #slope
@@ -37,20 +31,15 @@ b0 = y_mean - (b1 * x_mean)
 ###evaluating the model
 ##predicted values and error
 #predicted values based on the estimated coefficients
-y_pred = []
-for i in x: y_pred += [b0 + (b1 * i)]
+y_pred = [b0 + (b1 * i) for i in x] 
 #residuals: difference between the observed outcome scores and the predicted values
-residuals = []
-for i in range(0, len(y)): residuals += [y[i] - y_pred[i]]
+residuals = [yvalue - y_pred[num] for num, yvalue in enumerate(y)]
 #residuals sqaured 
-residuals2 = []
-for i in residuals: residuals2 += [i ** 2]
+residuals2 = [i ** 2 for i in residuals]
 #model error
-model_error = []
-for i in y_pred: model_error += [i - y_mean]
+model_error = [i - y_mean for i in y_pred]
 #model error sqaured
-model_error2 = []
-for i in model_error: model_error2 += [i ** 2]
+model_error2 = [i ** 2 for i in model_error] 
 
 ##overall model fit
 #residual sum of squares
@@ -63,11 +52,11 @@ tss = rss + mss
 rse = (rss/(len(y)-2)) ** (1/2.0)
 #r-squared value
 r_sq = (tss - rss)/tss
-
+#standard error of intercept and slope 
 b0_se = rse * (((1/len(x)) + (x_mean**2/sum(x_dev2))) ** (1/2.0))
 b1_se = rse/((sum(x_dev2) ** (1/2.0)))
-
-f = (mss/1)/(rss/(len(x)-2)) #f-statistic 
+#f-statistic
+f = (mss/1)/(rss/(len(x)-2))
 #with simiple linear regressions the f-statistic can be found by squaring the t-statistic for the slope
 
 ####assessing predictive value
