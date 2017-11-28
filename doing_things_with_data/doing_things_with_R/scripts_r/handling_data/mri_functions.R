b1f400f90046c0cc82ab5606e1cba6c5e1cda973
Charles Guthrie <charles.guthrie3@gmail.com>
2017-11-28 14:49:11 -0500

    updates to mri function in R

--- doing_things_with_data/doing_things_with_R/scripts_r/handling_data/mri_functions.R
+++ doing_things_with_data/doing_things_with_R/scripts_r/handling_data/mri_functions.R
@@ -1,4 +1,6 @@
-####multiple response item function####
+####multiple response item functions####
+
+
 ###select one option 
 mri_selectone <- function(data, response_labels = colnames(data), combined_variable_name = "new"){
   data[data!=1] <- NA 
@@ -8,9 +10,13 @@ mri_selectone <- function(data, response_labels = colnames(data), combined_varia
   return(data[length(data)])
 }
 
-###select all that apply option 
+###select all that apply response items
 selectall <- function(data){
-  #inner functions
+  ####NOTES####
+  #takes a data frame of binary (0,1: 1 = selected) values
+  #returns single variable of string conbinations of selected values
+  
+  ####inner functions####
   f1 <- function(x){
     out <- NULL
     for(i in 1:length(x)){
@@ -23,6 +29,8 @@ selectall <- function(data){
     out <- paste(out, collapse = " ")
     return(out)
   }
+  
+  ####process####
   #remove na
   data[is.na(data)] <- 0
   #convert all non-1 values to 0
@@ -32,10 +40,34 @@ selectall <- function(data){
   for(r in 1:nrow(data)){
     results <- append(x = results, values = f1(data[r,]))
   }
+  
   #convert to dataframe
   results <- data.frame(results, stringsAsFactors = F)
   #replace blank cells with NA
   results[-grep("[a-z]", results$results),] <- NA
+  return(results)
+}
+
+####select ranked items 
+selectrank <- function(data, rankno = 1){
+  #inner function 
+  f1 <- function(x){
+    out <- NULL
+    for(i in 1:length(x)){
+      if(x[i] == rankno){
+        out <- colnames(x[i])
+      }
+      if(x[i] == 0){
+        out <- NA
+      }
+    }
+    return(out)
+  }
+  data[is.na(data)] <- 0
+  results <- vector()
+  for(r in 1:nrow(data)){
+    results <- append(x = results, values = f1(data[r,]))
+  }
   return(results)
 }
 
@@ -52,8 +84,35 @@ group_and_factor <- function(variable, b = 2){
   return(cut_var)
 }
 
-###create factor variable from progressive yes/no (binary) responses 
-mri_series <- function(data, response_labels){
+####split the variables with other text
+other_split <- function(data, lev_nos, labs){
+  #result vectors 
+  r_factor <- vector()
+  r_other <- vector()
+  #for loop 
+  for(i in data){
+    if(!is.na(i)){
+      if(nchar(i) == 1){
+        r_factor <- append(x = r_factor, values = i)
+        r_other <- append(x = r_other, values = NA)
+      }else{
+        r_factor <- append(x = r_factor, values = max(lev_nos))
+        r_other <- append(x = r_other, values = i)
+      }
+    }else{
+      r_factor <- append(x = r_factor, values = NA)
+      r_other <- append(x = r_other, values = NA)
+    }
+  }
+  #convert to factor variable 
+  r_factor <- factor(x = r_factor, levels = lev_nos, labels = labs) 
+  #combine result vectors 
+  r_all <- data.frame(r_factor, r_other, stringsAsFactors = F)
+  return(r_all)
+}
+
+####create factor variable from progressive yes/no (binary) responses 
+series <- function(data, response_labels){
   #process data# 
   data[data!=1] <- 2 
   colnames(data) <- response_labels
